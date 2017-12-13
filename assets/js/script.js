620678fa3becdbc53fba1e440d148f90ff755f29
Steve Walker <captnwalker@gmail.com>
2017-12-13 00:41:03 -0500

    re-worked layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-  
-  
+
+    var timeleft = 30;
+    var downloadTimer = setInterval(function(){
+    timeleft--;
+
+    document.getElementById('timer').textContent = timeleft;
+    
+    if(timeleft <= 0)
+        clearInterval(downloadTimer);
+    },1000)
+  });
